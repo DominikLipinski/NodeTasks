b90053bd4b78743d0d6a6ce3078ef86a758554ef
Dominik Lipinski <maboonga@gmail.com>
2017-10-22 10:24:20 +0200

    require

---
+++ firstModule/app.js
@@ -0,0 +1,3 @@
+var newCounter = require ('./counter');
+
+console.log(newCounter(['apple', 'plum', 'banana']));
---
+++ firstModule/counter.js
@@ -0,0 +1,5 @@
+var myCounter = function(arr){
+    return'There are ' + arr.length + ' elements here.'
+}
+
+module.exports = myCounter;
