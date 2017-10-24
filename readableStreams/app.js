496d8009073b39f1ea7f18fa34ac1f47bf189ffe
Dominik Lipinski <maboonga@gmail.com>
2017-10-24 21:01:11 +0200

    pipes

--- readableStreams/app.js
+++ readableStreams/app.js
@@ -1,26 +1,22 @@
 var http = require('http');
     fs = require('fs');
-    myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
-    myWriteStream = fs.createWriteStream(__dirname + '/newFile.txt', 'utf8');
 
+
+/*
 myReadStream.on('data', function(chunk){ 
     console.log("We received new chunk");
     myWriteStream.write(chunk);
 })
+*/
 
-
-
-
-
-
-/*
 var http = require('http');
     server = http.createServer(function(req, res){
         console.log('We made request: ' +req.url);
         res.writeHead(200, {'Content-Type': 'text/plain'})
-        res.end('Hey there');
+        
+    myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
+    myReadStream.pipe(res);
     });
 
 server.listen(3000, '127.0.0.1');
 console.log('listening to 3000 server...');
-*/
