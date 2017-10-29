29e18c97bf157ad20ef3ac5441d09e82173ade69
Dominik Lipinski <maboonga@gmail.com>
2017-10-29 17:01:57 +0100

    to do list - js logic part

--- todoNode/controllers/todoController.js
+++ todoNode/controllers/todoController.js
@@ -1,11 +1,18 @@
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+var data = [{task: 'get milk'}, {task: 'buy eggs'}, {task: 'go to sleep'}];
+
+
 module.exports = function(app) {
 
 app.get('/todo', function(req, res) {
-    res.render('todo');
+    res.render('todo', {todos: data});
 });
 
-app.post('/todo', function(req, res) {
-    
+app.post('/todo', urlencodedParser, function(req, res) {
+    data.push(req.body);
+    // res.render('todo', {todos: data});
+    res.json(data);
     });
 
 app.delete('/todo', function(req, res) {
